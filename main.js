82031a11df023ce6585becd893beb25afd896f07
Jayasheel Vinay <jayasheelvinayj@gmail.com>
2024-04-22 19:15:07 +0530

    Working Carousel with L-R buttons

--- main.js
+++ main.js
@@ -3,49 +3,10 @@
 const left = document.querySelector('.arrow-left');
 const right = document.querySelector('.arrow-right');
 const slider = document.querySelector('.slides-container');
-const images = document.querySelectorAll('.img');
+const images = document.querySelectorAll('.image');
 
 let slideNumber = 1;
 const length = images.length;
-
-//Navigation Dots Section
-
-const bottom = document.querySelector('.bottom');
-
-for(let i = 0; i < length; i++){
-    const div = document.createElement('div');
-    div.className = 'dot';
-    bottom.appendChild(div);
-}
-
-const dots = document.querySelectorAll('.dot');
-
-dots[0] .style.backgroundColor = 'black';
-
-//Changing Slide on Dot Click
-const resetBg = () => {
-    dots.forEach(dot => {
-        dot.style.backgroundColor = '#afafaf';
-    });
-};
-
-//Changing Slide on Dot Click
-dots.forEach((dot, index) => {
-    dot.addEventListener('click', () => {
-        resetBg();
-        dot.style.backgroundColor = 'black';
-        slider.style.transform = `translateX(-${index*1240}px)`;
-        slideNumber = index+1;
-    });
-});
-
-//Changing Dot Color on Slide Change
-const changeColor = () => {
-    resetBg();
-    dots[slideNumber-1].style.backgroundColor = 'black';
-}
-
-//Navigation Button Section-2
 
 //Next Slide function
 const nextSlide = () => {
@@ -70,7 +31,6 @@ const getLastSlide = () => {
     slider.style.transform = `translateX(-${(length-1)*1240}px)`;
         slideNumber = length;;
 };
-
 
 //Slide Changing done using ternary operator 
 //Which does the same thing as if else statement
