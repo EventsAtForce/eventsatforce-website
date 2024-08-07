5da83520d1044674ef051098bcf15813ea04badf
Jayasheel Vinay <jayasheelvinayj@gmail.com>
2024-08-07 23:03:24 +0530

    add: locomotive scroll (beta)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Routes, Route } from "react-router-dom";
+import LocomotiveScroll from 'locomotive-scroll';
 import './App.css'
 import Home from './components/Home'
 import Navbar from './components/Navbar'
 
 function App() {
 
+  const locomotiveScroll = new LocomotiveScroll();
+  
   return (
     <>
       <Navbar />
