d9e91151448a92228d74f3f2bad8d1b0a9719c0e
Jayasheel Vinay <jayasheelvinayj@gmail.com>
2025-01-21 07:04:10 +0530

    fix: build errors

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import App from './App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
