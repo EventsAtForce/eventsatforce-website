02c86eb89577708abfce7e7eb38fa8468cbc632e
akatwrk <akatwrk18@gmail.com>
2026-02-21 23:50:06 +0530

    feat: Implement EventDetails component with structured layout and styling

--- src/components/EventDetails/EventDetails.jsx
+++ src/components/EventDetails/EventDetails.jsx
@@ -3,8 +3,32 @@ import './EventDetails.css'
 
 const EventDetails = () => {
   return (
-    <div className='EventDetails'>
-      Event Details
+    <div class="event-card">
+        <div class="cover-img">
+        
+        </div>
+  
+        <div class="details-row">
+            <span>name</span>
+            <span>date</span>
+            <span>venue</span>
+        </div>
+
+        <div class = "venue">
+            <span>Robosoccer</span>
+            <span>March 13</span>
+            <span>Amphitheatre</span>
+        </div>
+
+        <div class="content-section">
+        <p class="label">description</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum illo quasi tempore laborum, porro autem maiores similique quia cupiditate! Perferendis quisquam obcaecati rerum necessitatibus voluptas ipsam corporis quas, excepturi facilis.</p>
+        </div>
+
+        <div class="content-section">
+        <p class="label">rules</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae iusto incidunt error deleniti cumque vel nemo voluptatum distinctio eos veritatis quisquam ratione amet corrupti sequi nulla, tempora facere ipsam doloremque.</p>
+        </div>
     </div>
   )
 }
